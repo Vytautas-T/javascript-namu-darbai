d36c6835f38685764bc3762cb920d8fcfc891223
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-13 15:00:44 +0300

    Formulė teisinga

--- uzduotis 6/script.js
+++ uzduotis 6/script.js
@@ -1,20 +1,15 @@
 window.onload = function () {
     let pirmaKaina = 100;
     let pirmaGalutineKaina = 275;
-    let antrakaina = 15.99;
+    let antraKaina = 15.99;
     let antraGalutineKaina = 16.99
 
     console.log(pakilimasProcentais(pirmaGalutineKaina,pirmaKaina))
-    console.log(nukritimasProcentais(antraGalutineKaina,antrakaina))
+    console.log(nukritimasProcentais(antraKaina,antraGalutineKaina))
 }
 
 function pakilimasProcentais (galutineKaina,kaina) {
-    return galutineKaina - kaina / kaina * 100 + "%";
+    return (galutineKaina - kaina) / kaina * 100 + "%";
 }
- 
-function nukritimasProcentais (kaina,galutineKaina) {
-    return (kaina - galutineKaina) / kaina * 100 + "%";
-}
-
 // procentu nukritimo apskaiciavimas
 //16.99 - 15.99 = 1 ÷ 16.99 = 0.058 * 100 = 5.8
