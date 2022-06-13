6343b156fad208a82bccd270685f4c09b6550b6e
Vytautas-T <105976888+Vytautas-T@users.noreply.github.com>
2022-06-13 15:14:51 +0300

    pataisytas pagal pavyzdy

--- uzduotis 6/script.js
+++ uzduotis 6/script.js
@@ -5,16 +5,9 @@ window.onload = function () {
     let antraGalutineKaina = 16.99
 
     console.log(pakilimasProcentais(pirmaGalutineKaina,pirmaKaina))
-    console.log(nukritimasProcentais(antraGalutineKaina,antrakaina))
+    console.log(pakilimasProcentais(antraGalutineKaina,antrakaina))
 }
 
 function pakilimasProcentais (galutineKaina,kaina) {
-    return galutineKaina - kaina / kaina * 100 + "%";
+    return (galutineKaina - kaina) / kaina * 100 + "%";
 }
- 
-function nukritimasProcentais (kaina,galutineKaina) {
-    return (kaina - galutineKaina) / kaina * 100 + "%";
-}
-
-// procentu nukritimo apskaiciavimas
-//16.99 - 15.99 = 1 ÷ 16.99 = 0.058 * 100 = 5.8
